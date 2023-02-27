e0bd12476e21f6d84d1b284b03afcfba68d8a8f2
Martijn <12066560+Kipjr@users.noreply.github.com>
2023-02-27 17:50:35 +0100

    Update Prepare-TestEnvironment.sh

--- .tests/Prepare-TestEnvironment.sh
+++ .tests/Prepare-TestEnvironment.sh
@@ -5,13 +5,14 @@ git clone https://github.com/Kipjr/docker-piwigo docker-piwigo
 function GetRandom(){
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 24 | head -n 1
 }
-__TEMPLATE__PASSWORD1=$(GetRandom)
-__TEMPLATE__PASSWORD2=$(GetRandom)
-__TEMPLATE__PASSWORD3=$(GetRandom)
-__TEMPLATE__PASSWORD4=$(GetRandom)
-declare -x __TEMPLATE__PASSWORD1
-declare -x __TEMPLATE__PASSWORD2
-declare -x __TEMPLATE__PASSWORD3
-declare -x __TEMPLATE__PASSWORD4
+
+__TEMPLATE__DB_ROOT_PASSWORD=$(GetRandom)
+__TEMPLATE__PWG_ADMIN_PASSWORD=$(GetRandom)
+__TEMPLATE_LDAP_ADMIN_PASSWORD=$(GetRandom)
+__TEMPLATE__LDAP_CONFIG_PASSWORD=$(GetRandom)
+declare -x __TEMPLATE__DB_ROOT_PASSWORD
+declare -x __TEMPLATE__PWG_ADMIN_PASSWORD
+declare -x __TEMPLATE_LDAP_ADMIN_PASSWORD
+declare -x __TEMPLATE__LDAP_CONFIG_PASSWORD
 
 envsubst < docker-piwigo/docker-compose.template > docker-compose.yml
