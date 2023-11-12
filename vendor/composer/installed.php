ce50747efc460f1f4c741b2d37e8012037519230
GitHub Action <action@github.com>
2023-11-12 19:56:38 +0000

    🦋 Normalized composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'kipjr/ldap_login',
         'pretty_version' => 'dev-dev',
         'version' => 'dev-dev',
-        'reference' => 'cf45e79733ea5571b0f4161241f384465ddec284',
+        'reference' => '9455814deb635184eb880edcabe49000f395cc3c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -49,7 +49,7 @@
         'kipjr/ldap_login' => array(
             'pretty_version' => 'dev-dev',
             'version' => 'dev-dev',
-            'reference' => 'cf45e79733ea5571b0f4161241f384465ddec284',
+            'reference' => '9455814deb635184eb880edcabe49000f395cc3c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
